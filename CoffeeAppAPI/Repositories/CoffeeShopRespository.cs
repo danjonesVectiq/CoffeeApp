26b5553908fb6e38d0965b744a295e858a6eda12
danjonesVectiq <112914348+danjonesVectiq@users.noreply.github.com>
2023-04-09 13:41:11 +1000

    about to refactor everything

--- CoffeeAppAPI/Repositories/CoffeeShopRespository.cs
+++ CoffeeAppAPI/Repositories/CoffeeShopRespository.cs
@@ -8,14 +8,29 @@ namespace CoffeeAppAPI.Repositories
 {
     public interface ICoffeeShopRepository : IRepository<CoffeeShop>
     {
-        // Add any CoffeeShop-specific methods here, if needed
+       // Task DeleteImageForCoffeeShopAsync(Guid id, string ImageUrl);
+        Task DeleteAsync(CoffeeShop coffeeShop);
     }
 
     public class CoffeeShopRepository : CosmosDbRepository<CoffeeShop>, ICoffeeShopRepository
     {
-        public CoffeeShopRepository(ICosmosDbService cosmosDbService)
+
+        private readonly IBlobStorageRepository _blobStorageRepository;
+        public CoffeeShopRepository(ICosmosDbService cosmosDbService, IBlobStorageRepository blobStorageRepository)
             : base(cosmosDbService, "Coffee", "/id", "CoffeeShop")
         {
+            _blobStorageRepository = blobStorageRepository;
         }
+
+        public async Task DeleteAsync(CoffeeShop coffeeShop)
+        {
+            if (coffeeShop != null && !string.IsNullOrEmpty(coffeeShop.ImageUrl))
+            {
+                await _blobStorageRepository.DeleteImageAsync(coffeeShop.id, coffeeShop.ImageUrl);
+            }
+            await base.DeleteAsync(coffeeShop.id);
+        }
+
     }
 }
+ 
